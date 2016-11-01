ff3199f830735ee286ea9ab51ad3cf9b75760de7
Donovan Allen <dallen.928@gmail.com>
2016-11-01 15:41:51 -0400

    adds trainers and main select page at /

--- app.rb
+++ app.rb
@@ -7,7 +7,13 @@ require_relative 'db/connection'
 
 # Load models
 require_relative 'models/pokemon'
+require_relative 'models/trainer'
 
+get '/' do
+  erb :"main/index"
+end
+
+#POKEMON#
 get '/pokemon' do
   @pokemons = Pokemon.all
   erb :"pokemons/index"
@@ -42,4 +48,40 @@ delete '/pokemon/:id' do
   @pokemon = Pokemon.find(params[:id])
   @pokemon.destroy
   redirect '/pokemon'
+end
+# TRAINERS #
+get '/trainers' do
+  @trainers = Trainer.all
+  erb :"trainers/index"
+end
+
+get '/trainers/new' do
+  erb :"trainers/new"
+end
+
+post '/trainers' do
+  @trainer = Trainer.create(params[:trainer])
+  redirect "/trainers/#{@trainer.id}"
+end
+
+get '/trainers/:id' do
+  @trainer = Trainer.find(params[:id])
+  erb :"trainers/show"
+end
+
+get '/trainers/:id/edit' do
+  @trainer = Trainer.find(params[:id])
+  erb :"trainers/edit"
+end
+
+put '/trainers/:id' do
+  @trainer = Trainer.find(params[:id])
+  @trainer.update(params[:trainer])
+  redirect "/trainers/#{@trainer.id}"
+end
+
+delete '/trainers/:id' do
+  @trainer = Trainer.find(params[:id])
+  @trainer.destroy
+  redirect '/trainers'
 end
--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,18 @@
 DROP TABLE IF EXISTS pokemons;
+DROP TABLE IF EXISTS trainers;
 
 CREATE TABLE pokemons(
   id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   poke_type TEXT,
   cp INT,
+  img_url TEXT,
+  trainer_id INT
+);
+
+CREATE TABLE trainers(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  level INT NOT NULL,
   img_url TEXT
 );

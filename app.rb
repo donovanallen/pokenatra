1ddc2e1356ac4901e8fabbf9181152164d310361
Donovan Allen <dallen.928@gmail.com>
2016-11-01 12:29:05 -0400

    mvp complete, styling does not work

--- app.rb
+++ app.rb
@@ -19,10 +19,27 @@ end
 
 post '/pokemon' do
   @pokemon = Pokemon.create(params[:pokemon])
-  redirect "/pokemon/#{@pokemon.name}"
+  redirect "/pokemon/#{@pokemon.id}"
 end
 
-get '/pokemon/:name' do
-  @pokemon = Pokemon.find_by(name: params[:name])
+get '/pokemon/:id' do
+  @pokemon = Pokemon.find(params[:id])
   erb :"pokemons/show"
+end
+
+get '/pokemon/:id/edit' do
+  @pokemon = Pokemon.find(params[:id])
+  erb :"pokemons/edit"
+end
+
+put '/pokemon/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.update(params[:pokemon])
+  redirect "/pokemon/#{@pokemon.id}"
+end
+
+delete '/pokemon/:id' do
+  @pokemon = Pokemon.find(params[:id])
+  @pokemon.destroy
+  redirect '/pokemon'
 end
--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS pokemons;
 
 CREATE TABLE pokemons(
+  id SERIAL PRIMARY KEY,
   name TEXT NOT NULL,
   poke_type TEXT,
   cp INT,
